909837602e9c445e6a5818f568f0a52c2465edac
Campionii <133364100+Campionii@users.noreply.github.com>
2024-03-07 08:25:27 -0300

    Add files via upload

---
+++ chat2/db.php
@@ -0,0 +1,17 @@
+<?php 
+
+
+// BANCO DE DADOS SERVIDOR LOCAL XAMPP
+$servidor = "localhost";
+$usuario = "root";
+$password = "";
+$bd = "chat2";  
+
+$conexao = new mysqli($servidor,$usuario,$password,$bd);
+
+function formatarData($data){
+    return date('H:i:s', strtotime($data));
+
+}
+
+ ?>
